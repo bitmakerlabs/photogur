f2a44c3ad2d6a7c6870110766c2a4551121d06ff
Julie Hache <elektrophile@gmail.com>
2015-02-04 23:12:42 -0500

    Creating some default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create!(
+  :title  => "The old church on the coast of White sea",
+  :artist => "Sergey Ershov",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+)
+
+Picture.create!(
+  :title  => "Sea Power",
+  :artist => "Stephen Scullion",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+)
+
+Picture.create!(
+  :title  => "Into the Poppies",
+  :artist => "John Wilhelm",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+)
