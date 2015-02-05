7d45c1bd6a0a1c0ef9064959a05334712d600b0f
Julie Hache <elektrophile@gmail.com>
2015-02-04 23:35:48 -0500

    Refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
   root 'pictures#index'
-  get 'pictures' => 'pictures#index'
 
-  post 'pictures' => 'pictures#create' # this is a new line of code
-  get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+  resources :pictures
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
