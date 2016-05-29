4b1829728dec5672b53b0e94e61fef7b7e8e5701
NatalieBlack <natalievictoriablack@gmail.com>
2016-05-29 13:27:28 -0400

    ccreate empty photogur project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  root 'pictures#index'
-
-  resources :pictures
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
