6a93b1ad1c30597183937c5976f723312558b9cc
zap-rowsdower <brock.whitbread@gmail.com>
2017-07-11 20:40:18 -0700

    set up base repo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,77 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+# Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Picture.create(
-  title:  'The old church on the coast of White sea',
-  artist: 'Sergey Ershov',
-  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg'
+  title:  'Scientist',
+  artist: 'Rick Sanchez',
+  url:    'https://images-na.ssl-images-amazon.com/images/M/MV5BNzYwYWYyYmMtNmY1YS00N2RmLWE0OTQtZTJlYjUxODNmMGZhXkEyXkFqcGdeQXVyNTkyMjE3NDU@._V1_.jpg'
 )
 
 Picture.create(
-  title:  'Sea Power',
-  artist: 'Stephen Scullion',
-  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg'
+  title:  'Grandson & Assistant',
+  artist: 'Morty Smith',
+  url:    'http://i.cdn.turner.com/asfix/repository//8a250ba13f865824013fc9db8b6b0400/thumbnail_6280423165162588467.jpg'
 )
 
 Picture.create(
-  title:  'Into the Poppies',
-  artist: 'John Wilhelm',
-  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg'
+  title:  'Veterinarian',
+  artist: 'Beth Smith',
+  url:    'http://i.cdn.turner.com/asfix/repository//8a250ba13f865824013fc9db8b6b0400/thumbnail_2185830388888274768.jpg'
+)
+
+Picture.create(
+  title:  'Adveritisng Consultant',
+  artist: 'Jerry Smith',
+  url:    'http://i.cdn.turner.com/asfix/repository//8a250ba13f865824013fc9db8b6b0400/thumbnail_6854564887113257193.jpg'
+)
+
+Picture.create(
+  title:  'Granddaughter',
+  artist: 'Summer Smith',
+  url:    'https://s-media-cache-ak0.pinimg.com/736x/d7/86/f6/d786f6636413e3faee4df78b2c7f9c91--crazy-scientist-stuff-and-thangs.jpg'
+)
+
+Picture.create(
+  title:  'One time Freedom Fighter',
+  artist: 'Birdperson',
+  url:    'https://i.ytimg.com/vi/b18KXP6KuWM/hqdefault.jpg'
+)
+
+Picture.create(
+  title:  'One time Freedom Fighter',
+  artist: 'Squanchy',
+  url:    'http://cdn.idigitaltimes.com/sites/idigitaltimes.com/files/styles/embed/public/2016/03/04/rick-and-morty-bird-person-season-3.jpg'
+)
+
+Picture.create(
+  title:  'Gear Wars Historian',
+  artist: 'Revolio "Gearhead" Clockberg, Jr.',
+  url:    'http://www.toonzone.net/fansites/blogicon/57082/master/1467427700.jpg'
+)
+
+Picture.create(
+  title:  'Star of Jan Quadrant Vincent 16',
+  artist: 'Jan Michael Vincent',
+  url:    'http://i.cdn.turner.com/adultswim/big/video/a/rickandmorty_ep208_003_Jan_Michael_Vincent.jpg'
+)
+
+Picture.create(
+  title:  'Theif/kleptomaniac',
+  artist: 'Stealy',
+  url:    'https://vignette3.wikia.nocookie.net/rickandmorty/images/f/f2/Stealy.png/revision/latest?cb=20150926185140'
+)
+
+Picture.create(
+  title:  'Host of the Personal Space Show',
+  artist: 'Philip Jacobs',
+  url:    'https://s-media-cache-ak0.pinimg.com/originals/54/3e/67/543e6734d58e1f7ba6f9fd26d69868ed.jpg'
+)
+
+Picture.create(
+  title:  'Author and son of Morty',
+  artist: 'Morty Jr.',
+  url:    'http://www.shahspace.com/R&M/ILP/Morty%20Jr.%20as%20a%20teenager.png'
 )
